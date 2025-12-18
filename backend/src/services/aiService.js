4e43e489b93e21b6088add8741d96b64806f2ca4
Primotion Studio <oyedelenewton@gmail.com>
2025-12-18 16:12:32 +0100

    first commit

--- backend/src/services/aiService.js
+++ backend/src/services/aiService.js
@@ -14,6 +14,7 @@ async function verifyFacesWithAI(liveImageBase64, referenceImageBase64) {
     const response = await axios.post(url, payload, { timeout: 50000 });
     return response.data;
   } catch (error) {
+    console.log("❌ Error occurred:", error);
     if (!error.response) {
       console.error("❌ Likely CORS or network error:", error.message);
     } else {
